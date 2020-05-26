0fe5fc79af54a196e3188e8bb51b451ef649cae8
Daniel Livingston <r.daniellivingston@gmail.com>
2020-05-26 21:33:24 +0530

    Kadane's Algorithm added

---
+++ Problems_Solved/Coding Blocks/Algo++ Course/Arrays/Maximum Subarray Efficient.cpp
@@ -0,0 +1,28 @@
+// This approach is using Kadane's Algorithm...
+// Time Complexity: O(N)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[1000], cumsum[1000];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    //Kadane's Algorithm
+    int max_sum = INT_MIN, current_sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        current_sum += arr[i];
+        max_sum = max(max_sum, current_sum);
+        if (current_sum < 0)
+        {
+            current_sum = 0;
+        }
+    }
+    cout << "Maximum sum is : " << max_sum << endl;
+}
