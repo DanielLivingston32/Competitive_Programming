d81911e8726b943e087398f38c6db135a1ffb92d
Daniel Livingston <r.daniellivingston@gmail.com>
2020-06-03 12:16:50 +0530

    Update problem

--- Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Arrays/Rain water harvesting.cpp
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Arrays/Rain water harvesting.cpp
@@ -3,6 +3,7 @@
 #include <climits>
 using namespace std;
 
+// The naive approach has a complexity of O(N^2)
 int NaiveApproach(int arr[], int n)
 {
     int total = 0;
@@ -23,6 +24,46 @@ int NaiveApproach(int arr[], int n)
     return total;
 }
 
+// From the naive approach if we note clearly both the left and right are repetitive and is calculated again and again increasing complexity
+// So inorder to make this better we optimise this part to Decrease complexity to O(N) and space complexity O(N)
+int EfficientApproach(int arr[], int n)
+{
+    int total = 0;
+    int left[n];
+    int right[n];
+    left[0] = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > left[i - 1])
+        {
+            left[i] = arr[i];
+        }
+        else
+        {
+            left[i] = left[i - 1];
+        }
+    }
+
+    right[n - 1] = arr[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (arr[i] > right[i + 1])
+        {
+            right[i] = arr[i];
+        }
+        else
+        {
+            right[i] = right[i + 1];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        total += min(left[i], right[i]) - arr[i];
+    }
+    return total;
+}
+
 int main()
 {
     int n;
@@ -32,5 +73,5 @@ int main()
     {
         cin >> arr[i];
     }
-    cout << NaiveApproach(arr, n);
+    cout << EfficientApproach(arr, n);
 }
