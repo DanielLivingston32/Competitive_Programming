5e68ad4a6d57f78b8f2c6f3602c5864cc9ed5f0b
Daniel Livingston <r.daniellivingston@gmail.com>
2020-06-03 12:14:14 +0530

    New section Arrangement with problems added in Arrays IB

---
+++ Problems_Solved/Problem-Solving-IB-Series/Arrays/Arrangement/Rotate matrix.cpp
@@ -0,0 +1,29 @@
+void Solution::rotate(vector<vector<int>> &A)
+{
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int n = A.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i > j)
+            {
+                swap(A[i][j], A[j][i]);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j < n / 2)
+            {
+                swap(A[i][j], A[i][n - j - 1]);
+            }
+        }
+    }
+}
