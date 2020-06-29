8ecdc3e52ff2ee0cc28dda7f1b44f3abc6889117
Daniel Livingston <r.daniellivingston@gmail.com>
2020-06-29 14:27:21 +0530

    Added new Problem in Challenges Fundamentals

---
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Pythogorean triplets.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	long int n;
+    cin>>n;
+    if(n==1 || n==2){
+        cout<<"-1";
+		return 0;
+    }
+    if(n%2==0){
+        long int m=n/2;
+        cout<<m*m-1<<" "<<m*m+1;
+    }else{
+        long int m=(n+1)/2;
+        long int t=(n-1)/2;
+        cout<<2*m*t<<" "<<m*m+t*t;
+    }
+}
