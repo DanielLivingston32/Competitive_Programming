5a282517f3c9f79ee6dbb3eade0d10540b3adb9d
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-04 22:30:06 +0530

    Added new Problem in Challenges Fundamentals

---
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Decimal to Binary.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    long long number;
+    cin>>number;
+    
+    long int binary = 0;
+    int cnt = 0;    
+
+    while(number){
+        int temp = number % 10;
+        binary += temp*pow(2,cnt);
+        cnt++;
+        number/=10;
+    }
+    
+    cout<<binary<<endl;
+}
