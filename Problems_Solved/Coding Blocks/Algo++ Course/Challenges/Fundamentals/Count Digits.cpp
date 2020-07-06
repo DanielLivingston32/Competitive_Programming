ded12eb02d84bc87a3b0b7d6802e343806933b5d
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-06 13:45:15 +0530

    Added new Problem in Challenges Fundamentals

---
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Count Digits.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    long int no;
+    int check_no,cnt = 0;
+    cin>>no>>check_no;
+    while(no){
+        if(no%10==check_no){
+            cnt++;
+        }
+        no/=10;
+    }
+    cout<<cnt;
+    
+}
