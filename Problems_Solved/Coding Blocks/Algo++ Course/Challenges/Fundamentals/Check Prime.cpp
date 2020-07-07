b9fe99088b8a9d97a5148f2197a49c3617549542
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-07 13:56:28 +0530

    Added new Problem in Challenges Fundamentals

--- Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Check Prime.cpp
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Check Prime.cpp
@@ -3,8 +3,33 @@
 using namespace std;
 
 int main(){
-    long int n;
-    if(Check_prime(n)){
-        
+    char ch;
+    cin>>ch;
+    while(ch!='X' && ch!='x'){
+        if(ch=='+'){
+            int n1,n2;
+            cin>>n1>>n2;
+            cout<<n1+n2<<endl;    
+        }else if(ch=='-'){
+            int n1,n2;
+            cin>>n1>>n2;
+            cout<<n1-n2<<endl;    
+        }else if(ch=='*'){
+            int n1,n2;
+            cin>>n1>>n2;
+            cout<<n1*n2<<endl;    
+        }else if(ch=='/'){
+            int n1,n2;
+            cin>>n1>>n2;
+            cout<<n1/n2<<endl;    
+        }else if(ch=='%'){
+            int n1,n2;
+            cin>>n1>>n2;
+            cout<<n1%n2<<endl;    
+        }else{
+            cout<<"Invalid operation. Try again."<<endl;
+        }
+        cin>>ch;
     }
+
 }
