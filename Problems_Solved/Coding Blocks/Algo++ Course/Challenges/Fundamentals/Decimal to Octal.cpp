ecbe6f8203362ccad35ec25fbb574d31e89af642
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-08 12:59:20 +0530

    Added new Problem in Challenges Fundamentals

---
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Decimal to Octal.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+
+using namespace std;
+
+int main(){
+    long int no;
+    cin>>no;
+    vector<int> ans;
+    while(no){
+        ans.push_back(no%8);
+        no/=8;
+    }
+    reverse(ans.begin(),ans.end());
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i];
+    }
+
+}
