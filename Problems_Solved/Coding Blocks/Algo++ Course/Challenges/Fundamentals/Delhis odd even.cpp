762b4a9fc91c1868b9421009f21ac586210a16b2
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-09 17:59:30 +0530

    Added new Problem in Challenges Fundamentals

---
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Delhis odd even.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		long int no;
+		cin>>no;
+		int even_sum = 0, odd_sum = 0;
+		while(no){
+			int temp = no % 10;
+			if(temp%2==0){
+				even_sum += temp;
+			}else{
+				odd_sum += temp;
+			}
+			no/=10;
+		}
+		if(even_sum % 4 == 0 || odd_sum % 3 == 0){
+			cout<<"Yes";
+		}else{
+			cout<<"No";
+		}
+		cout<<endl;
+	}
+}
