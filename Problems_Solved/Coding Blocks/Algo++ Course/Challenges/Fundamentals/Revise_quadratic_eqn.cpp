74f8ec9d72dff4347df0054de83e053e1cdfef5f
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-10 15:09:54 +0530

    New Challenge Section

---
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Revise_quadratic_eqn.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    int discrim =  b*b - 4*a*c;
+    
+
+    if(discrim > 0){
+        cout<<"Real and Distinct"<<endl;
+        cout<<(-b - sqrt(discrim))/2*a << " "<<(-b + sqrt(discrim))/2*a;
+    }else if(discrim == 0){
+        cout<<"Real and Equal"<<endl;
+        cout<<(-b + sqrt(discrim))/2*a << " " << (-b + sqrt(discrim))/2*a;
+    }else{
+        cout<<"Imaginary";
+    }
+}
