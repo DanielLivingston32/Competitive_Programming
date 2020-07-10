37e46f76d1dc56300fc49b900fbc3491d51bdf41
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-10 15:26:12 +0530

    New Problem in Challenges  Fundamentals

---
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Von Nueman loves binary.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long number;
+        cin>>number;
+        
+        long int binary = 0;
+        int cnt = 0;    
+
+        while(number){
+            int temp = number % 10;
+            binary += temp*pow(2,cnt);
+            cnt++;
+            number/=10;
+        }
+        
+        cout<<binary<<endl;
+    }
+}
