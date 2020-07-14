d753e84b8cf7ee672cae6f507007dd7c5493e6bf
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-14 10:14:54 +0530

    Modified Problems in Challenges Fundamentals

--- Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Check Prime.cpp
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Check Prime.cpp
@@ -1,17 +1,19 @@
-#include<iostream>
-#include<cmath>
-
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main(){
+int main()
+{
     long int no;
-    cin>>no;
-    for(int i=2;i<sqrt(no);i++){
-        if(no % i == 0){
-            cout<<"Not Prime";
+    cin >> no;
+    for (int i = 2; i < sqrt(no); i++)
+    {
+        if (no % i == 0)
+        {
+            cout << "Not Prime";
             return 0;
         }
     }
-    cout<<"Prime";
+    cout << "Prime";
 }
