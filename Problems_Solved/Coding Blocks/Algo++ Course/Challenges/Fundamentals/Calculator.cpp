76c479485b71d1456e26a27add5ac08c2d840311
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-14 10:14:46 +0530

    Modified Problems in Challenges Fundamentals

--- Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Calculator.cpp
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Calculator.cpp
@@ -1,35 +1,47 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
     char ch;
-    cin>>ch;
-    while(ch!='X' && ch!='x'){
-        if(ch=='+'){
-            int n1,n2;
-            cin>>n1>>n2;
-            cout<<n1+n2<<endl;    
-        }else if(ch=='-'){
-            int n1,n2;
-            cin>>n1>>n2;
-            cout<<n1-n2<<endl;    
-        }else if(ch=='*'){
-            int n1,n2;
-            cin>>n1>>n2;
-            cout<<n1*n2<<endl;    
-        }else if(ch=='/'){
-            int n1,n2;
-            cin>>n1>>n2;
-            cout<<n1/n2<<endl;    
-        }else if(ch=='%'){
-            int n1,n2;
-            cin>>n1>>n2;
-            cout<<n1%n2<<endl;    
-        }else{
-            cout<<"Invalid operation. Try again."<<endl;
+    cin >> ch;
+    while (ch != 'X' && ch != 'x')
+    {
+        if (ch == '+')
+        {
+            int n1, n2;
+            cin >> n1 >> n2;
+            cout << n1 + n2 << endl;
         }
-        cin>>ch;
+        else if (ch == '-')
+        {
+            int n1, n2;
+            cin >> n1 >> n2;
+            cout << n1 - n2 << endl;
+        }
+        else if (ch == '*')
+        {
+            int n1, n2;
+            cin >> n1 >> n2;
+            cout << n1 * n2 << endl;
+        }
+        else if (ch == '/')
+        {
+            int n1, n2;
+            cin >> n1 >> n2;
+            cout << n1 / n2 << endl;
+        }
+        else if (ch == '%')
+        {
+            int n1, n2;
+            cin >> n1 >> n2;
+            cout << n1 % n2 << endl;
+        }
+        else
+        {
+            cout << "Invalid operation. Try again." << endl;
+        }
+        cin >> ch;
     }
-
 }
