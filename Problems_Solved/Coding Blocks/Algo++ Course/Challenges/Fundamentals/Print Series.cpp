77875f834cebbb5c1fdda8075bf762d9deabb74b
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-15 10:16:25 +0530

    Modified Problems in Challenges Fundamentals

--- Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Print Series.cpp
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Print Series.cpp
@@ -1,14 +1,16 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
-    int min,max,step;
-    cin>>min>>max>>step;
-    int fahren = (min-32)*5/9;
-    while(min<=max){
-        cout<<min<<" "<<fahren<<endl;
-        min+=step;
-		fahren = (min-32)*5/9;
+int main()
+{
+    int min, max, step;
+    cin >> min >> max >> step;
+    int fahren = (min - 32) * 5 / 9;
+    while (min <= max)
+    {
+        cout << min << " " << fahren << endl;
+        min += step;
+        fahren = (min - 32) * 5 / 9;
     }
 }
