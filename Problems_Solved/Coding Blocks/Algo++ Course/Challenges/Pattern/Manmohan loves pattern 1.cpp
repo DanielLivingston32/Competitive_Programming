f9577df295ebc56c66dc0c121fcb0982c086f417
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-16 10:18:09 +0530

    Added new Problems in Challenges

--- Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Pattern/Manmohan loves pattern 1.cpp
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Pattern/Manmohan loves pattern 1.cpp
@@ -1,26 +1,33 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main() {
+int main()
+{
 	int n;
-	cin>>n;
-	for(int i=1;i<=n;i++){
-		if(i%2==1){
-			int cnt=0;
-			while(cnt<i){
-				cout<<"1";
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 2 == 1)
+		{
+			int cnt = 0;
+			while (cnt < i)
+			{
+				cout << "1";
 				cnt++;
 			}
-			cout<<endl;
-		}else{
-			cout<<"1";
-			int cnt=1;
-			while(cnt<i-1){
-				cout<<"0";
+			cout << endl;
+		}
+		else
+		{
+			cout << "1";
+			int cnt = 1;
+			while (cnt < i - 1)
+			{
+				cout << "0";
 				cnt++;
 			}
-			cout<<"1"<<endl;
+			cout << "1" << endl;
 		}
 	}
 	return 0;
