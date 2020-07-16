25a74108acfa93962f6a3ba3281c6ec849a125d0
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-16 10:17:34 +0530

    Modified Problems in Challenges

--- Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Pythogorean triplets.cpp
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Pythogorean triplets.cpp
@@ -1,20 +1,25 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
-	long int n;
-    cin>>n;
-    if(n==1 || n==2){
-        cout<<"-1";
-		return 0;
+int main()
+{
+    long int n;
+    cin >> n;
+    if (n == 1 || n == 2)
+    {
+        cout << "-1";
+        return 0;
     }
-    if(n%2==0){
-        long int m=n/2;
-        cout<<m*m-1<<" "<<m*m+1;
-    }else{
-        long int m=(n+1)/2;
-        long int t=(n-1)/2;
-        cout<<2*m*t<<" "<<m*m+t*t;
+    if (n % 2 == 0)
+    {
+        long int m = n / 2;
+        cout << m * m - 1 << " " << m * m + 1;
+    }
+    else
+    {
+        long int m = (n + 1) / 2;
+        long int t = (n - 1) / 2;
+        cout << 2 * m * t << " " << m * m + t * t;
     }
 }
