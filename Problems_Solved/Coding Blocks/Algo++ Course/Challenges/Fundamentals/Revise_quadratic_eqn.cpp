d469e1145d2d9896f5c976e18543f898f5e46c0c
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-16 10:17:43 +0530

    Modified Problems in Challenges

--- Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Revise_quadratic_eqn.cpp
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Fundamentals/Revise_quadratic_eqn.cpp
@@ -1,22 +1,27 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main(){
-    int a,b,c;
-    cin>>a>>b>>c;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
 
-    int discrim =  b*b - 4*a*c;
-    
+    int discrim = b * b - 4 * a * c;
 
-    if(discrim > 0){
-        cout<<"Real and Distinct"<<endl;
-        cout<<(-b - sqrt(discrim))/2*a << " "<<(-b + sqrt(discrim))/2*a;
-    }else if(discrim == 0){
-        cout<<"Real and Equal"<<endl;
-        cout<<(-b + sqrt(discrim))/2*a << " " << (-b + sqrt(discrim))/2*a;
-    }else{
-        cout<<"Imaginary";
+    if (discrim > 0)
+    {
+        cout << "Real and Distinct" << endl;
+        cout << (-b - sqrt(discrim)) / 2 * a << " " << (-b + sqrt(discrim)) / 2 * a;
+    }
+    else if (discrim == 0)
+    {
+        cout << "Real and Equal" << endl;
+        cout << (-b + sqrt(discrim)) / 2 * a << " " << (-b + sqrt(discrim)) / 2 * a;
+    }
+    else
+    {
+        cout << "Imaginary";
     }
 }
