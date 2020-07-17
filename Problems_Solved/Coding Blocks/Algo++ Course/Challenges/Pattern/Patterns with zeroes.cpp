fed9de313142eb98e29ae445c3c4f49a06e7a6fa
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-17 10:18:55 +0530

    Modified Problems in Challenges

--- Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Pattern/Patterns with zeroes.cpp
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Pattern/Patterns with zeroes.cpp
@@ -1,22 +1,29 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    for(int i = 1;i<=n;i++){
-        if(i==1){
-            cout<<"1"<<endl;
-        }else{
-            cout<<i<<"\t";
-            int cnt=1;
-            while(cnt<i-1){
-                cout<<"0"<<"\t";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == 1)
+        {
+            cout << "1" << endl;
+        }
+        else
+        {
+            cout << i << "\t";
+            int cnt = 1;
+            while (cnt < i - 1)
+            {
+                cout << "0"
+                     << "\t";
                 cnt++;
             }
-            cout<<i;
-            cout<<endl;
+            cout << i;
+            cout << endl;
         }
-	}
+    }
 }
