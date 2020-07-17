8a8ec71b3cf45c13e8723ab4fed07068fa56f91b
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-17 10:18:45 +0530

    Modified Problems in Challenges

--- Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Pattern/Pattern Mountain.cpp
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Pattern/Pattern Mountain.cpp
@@ -1,45 +1,54 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int temp;
-    for(int i=1,j=n;i<n;i++){
-        if(i>1){
-            temp=1;
-            while(temp<i){
-                cout<<temp<<"\t";
+    for (int i = 1, j = n; i < n; i++)
+    {
+        if (i > 1)
+        {
+            temp = 1;
+            while (temp < i)
+            {
+                cout << temp << "\t";
                 temp++;
             }
         }
-        cout<<i;
-        int cnt=0;
-        while(cnt<(j-1)*2){
-            cout<<"\t";
+        cout << i;
+        int cnt = 0;
+        while (cnt < (j - 1) * 2)
+        {
+            cout << "\t";
             cnt++;
         }
         j--;
-        cout<<i;
-        if(i>1){
-            temp=i-1;
-            while(temp>=1){
-                cout<<"\t"<<temp;
+        cout << i;
+        if (i > 1)
+        {
+            temp = i - 1;
+            while (temp >= 1)
+            {
+                cout << "\t" << temp;
                 temp--;
             }
         }
-		cout<<endl;
+        cout << endl;
     }
-	int final_cnt=1;
-	while(final_cnt<n){
-		cout<<final_cnt<<"\t";
-		final_cnt++;
-	}
-	cout<<final_cnt;
-	final_cnt=n-1;
-	while(final_cnt>=1){
-		cout<<"\t"<<final_cnt;
-		final_cnt--;
-	}
+    int final_cnt = 1;
+    while (final_cnt < n)
+    {
+        cout << final_cnt << "\t";
+        final_cnt++;
+    }
+    cout << final_cnt;
+    final_cnt = n - 1;
+    while (final_cnt >= 1)
+    {
+        cout << "\t" << final_cnt;
+        final_cnt--;
+    }
 }
