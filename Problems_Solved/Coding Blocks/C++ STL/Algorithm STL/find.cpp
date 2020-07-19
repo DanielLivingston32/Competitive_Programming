891c03d3bdcc8744840618387091729052c3dd07
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-19 20:51:21 +0530

    Added Algorithm STL

---
+++ Problems_Solved/Coding Blocks/C++ STL/Algorithm STL/find.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {3, 68, 48, 10, 75};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Please enter the element to check if it is present in the given array or not:" << endl;
+    int key;
+    cin >> key;
+
+    auto it = find(arr, arr + n, key);
+    cout << it - arr << endl;
+}
