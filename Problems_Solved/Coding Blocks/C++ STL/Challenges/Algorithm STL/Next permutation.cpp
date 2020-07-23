972e84c4e48dfbe3e4dc4a2d599f084734d1435e
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-23 21:51:27 +0530

    New section added

---
+++ Problems_Solved/Coding Blocks/C++ STL/Challenges/Algorithm STL/Next permutation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        next_permutation(arr, arr + n);
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
