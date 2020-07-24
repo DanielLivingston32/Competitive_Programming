527d3dc0c0b1c44690e3b459f77183b98156b6fc
Daniel Livingston <r.daniellivingston@gmail.com>
2020-07-24 22:48:08 +0530

    Leetcode Array Problem added

---
+++ Problems_Solved/Leetcode/Array Problems/1313. Decompress Run-Length Encoded List.cpp
@@ -0,0 +1,18 @@
+// Time complexity: O(Frequency+n)
+class Solution
+{
+public:
+    vector<int> decompressRLElist(vector<int> &nums)
+    {
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i += 2)
+        {
+            while (nums[i])
+            {
+                ans.push_back(nums[i + 1]);
+                nums[i]--;
+            }
+        }
+        return ans;
+    }
+};
