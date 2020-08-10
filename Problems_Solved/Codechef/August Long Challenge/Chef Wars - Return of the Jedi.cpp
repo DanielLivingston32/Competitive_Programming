792ec78aee213a1cbfb0b2c9bec1d5fe1cd112fc
Daniel Livingston <r.daniellivingston@gmail.com>
2020-08-10 18:56:42 +0530

    Contest Problem added

---
+++ Problems_Solved/Codechef/August Long Challenge/Chef Wars - Return of the Jedi.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        long int health, power;
+        long int total_attack=0;
+        cin>>health>>power;
+        while (power) {
+            total_attack+=power;
+            power/=2;
+        }
+        if (total_attack>=health) {
+            cout<<1;
+        }
+        else {
+            cout<<0;
+        }
+        cout<<endl;
+    }
+}
