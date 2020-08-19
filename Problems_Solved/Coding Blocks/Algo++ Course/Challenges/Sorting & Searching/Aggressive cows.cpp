7c1889f00c57fb03188187439c686463c3b62071
Daniel Livingston <r.daniellivingston@gmail.com>
2020-08-19 05:57:11 +0530

    New Problem added

---
+++ Problems_Solved/Coding Blocks/Algo++ Course/Challenges/Sorting & Searching/Aggressive cows.cpp
@@ -0,0 +1,71 @@
+// Time Complexity: O(NLog N)
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool isPossible(long int arr[], long int n, long int mid, long int c)
+{
+    // Current variable to store the position of the previous cow
+    long int current = arr[0];
+    // Iterating through the stall array
+    for (int i = 1; i < n; i++)
+    {
+        // If c becomes one that means all cows have been places so return true
+        if (c == 1)
+        {
+            return true;
+        }
+        else
+        {
+            // Check if the space seperation between the previous placed cow and the current stall is equal to or greater than mid.
+            // If it is greater then the current cow and the total cows remaining are updated.
+            if (arr[i] - current >= mid)
+            {
+                current = arr[i];
+                c--;
+            }
+        }
+    }
+    // If the entire array has been iterated then this will be used...
+    if (c == 1)
+    {
+        return true;
+    }
+    return 0;
+}
+
+int main()
+{
+    // Get input of total no of stalls and cows
+    int n, c;
+    cin >> n >> c;
+
+    // Get input array of stalls
+    long int stalls[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> stalls[i];
+    }
+    // Sort the stalls array
+    sort(stalls, stalls + n);
+    // Using binary search to choose the best space seperation for the cows
+    long int start = 0;
+    long int end = stalls[n - 1] - stalls[0];
+    long int mid, ans = -1;
+    while (start <= end)
+    {
+        mid = (start + end) / 2;
+        // If this space seperation is valid then update the ans and set the start greater than mid to search for higher possible answers.
+        if (isPossible(stalls, n, mid, c))
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    cout << ans;
+}
