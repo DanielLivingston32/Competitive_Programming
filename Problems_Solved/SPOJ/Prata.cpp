a655011756ee111b0309ecff325d1d6858df2d5c
Daniel Livingston <r.daniellivingston@gmail.com>
2020-08-23 13:27:08 +0530

    New Problem Added

--- Problems_Solved/SPOJ/Prata.cpp
+++ Problems_Solved/SPOJ/Prata.cpp
@@ -2,18 +2,22 @@
 
 using namespace std;
 
-bool isPossible(int rankOfCooks[],long int mid,int n,int c){
+bool isPossible(int rankOfCooks[], long int mid, int n, int c)
+{
     int total_parathas = 0;
-    for(int i=0;i<c;i++){
+    for (int i = 0; i < c; i++)
+    {
         int r = rankOfCooks[i];
-        int time = r,idx = 2;
-        while(time<=mid){
+        int time = r, idx = 2;
+        while (time <= mid)
+        {
             total_parathas++;
-            time+=idx*r;
+            time += idx * r;
             idx++;
         }
     }
-    if(total_parathas>=n){
+    if (total_parathas >= n)
+    {
         return true;
     }
     return false;
@@ -45,15 +49,16 @@ int main()
         while (start <= end)
         {
             mid = (start + end) / 2;
-            cout<<"MID: "<<mid<<endl;
             if (isPossible(rankOfCooks, mid, n, c))
             {
                 ans = mid;
                 end = mid - 1;
-            }else{
-                start = mid+1;
+            }
+            else
+            {
+                start = mid + 1;
             }
         }
-        cout<<ans;
+        cout << ans << endl;
     }
 }
