dcb6aa6ce5d3e207da27a308ccda8bb65b29684d
Daniel Livingston <r.daniellivingston@gmail.com>
2020-08-24 19:50:14 +0530

    New Problem Added

---
+++ Problems_Solved/GeeksForGeeks/Topic-wise-solution/Bit Magic/Set Bit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, ans = 0;
+        cin >> n;
+        while (n)
+        {
+            ans++;
+            n = n & (n - 1);
+        }
+
+        cout << ans << endl;
+    }
+    return 0;
+}
