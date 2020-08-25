cddfc03c080940f4f4aeae0c9d42f9723680f54c
Daniel Livingston <r.daniellivingston@gmail.com>
2020-08-25 10:44:43 +0530

    Error corrected

--- Problems_Solved/Codechef/August Cook Off/Chef and work.cpp
+++ Problems_Solved/Codechef/August Cook Off/Chef and work.cpp
@@ -1,35 +1,47 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int findMinimumRounds(int arr[],int n,int k){
-    int sum_till_now = 0;
+int findMinimumRounds(int arr[], int n, int k)
+{
+    int sum_till_now = arr[0];
+    if (sum_till_now > k)
+    {
+        return -1;
+    }
     int trips = 1;
-    for(int i=0;i<n;i++){
-        if(arr[i]>k){
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > k)
+        {
             return -1;
         }
-        if(sum_till_now+arr[i]<=k){
-            sum_till_now+=arr[i];
-        }else{
+        if (sum_till_now + arr[i] <= k)
+        {
+            sum_till_now += arr[i];
+        }
+        else
+        {
             trips++;
-            sum_till_now=0;
+            sum_till_now = arr[i];
         }
     }
     return trips;
 }
 
-
-int main(){
+int main()
+{
     int t;
-    cin>>t;
-    while(t--){
-        int n,k;
-        cin>>n>>k;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
         int weights[n];
-        for(int i=0;i<n;i++){
-            cin>>weights[i];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> weights[i];
         }
-        cout<<findMinimumRounds(weights,n,k);
+        cout << findMinimumRounds(weights, n, k) << endl;
     }
 }
