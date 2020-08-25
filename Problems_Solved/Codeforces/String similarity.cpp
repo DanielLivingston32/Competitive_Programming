1ad4a562d76dbff6e2d75e65dc55866fb3f9d9a1
Daniel Livingston <r.daniellivingston@gmail.com>
2020-08-25 23:52:30 +0530

    New Problem Added

---
+++ Problems_Solved/Codeforces/String similarity.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string input;
+        cin>>input;
+        cout<<input;
+    }
+}
