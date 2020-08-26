82136a0d83edf9c7ef48bf1ead65b3818da36e7a
Daniel Livingston <r.daniellivingston@gmail.com>
2020-08-26 11:25:53 +0530

    New Problem Added

--- Problems_Solved/Codeforces/String similarity.cpp
+++ Problems_Solved/Codeforces/String similarity.cpp
@@ -1,16 +1,29 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(){
+int main()
+{
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         int n;
-        cin>>n;
+        cin >> n;
         string input;
-        cin>>input;
-        cout<<input;
+        cin >> input;
+        string result = "";
+        int start_index = 0;
+        int end_index = n - 1;
+        int i = 0;
+        while (end_index < input.size())
+        {
+            result += input[start_index + i];
+            start_index++;
+            end_index++;
+            i++;
+        }
+        cout << result << endl;
     }
 }
