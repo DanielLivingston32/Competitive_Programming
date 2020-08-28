4d8a2e1ef4482217f620bc93551bc6520c58ddbc
Daniel Livingston <r.daniellivingston@gmail.com>
2020-08-28 22:14:26 +0530

    New Problem added

---
+++ Problems_Solved/GeeksForGeeks/Topic-wise-solution/Bit Magic/Play with Or.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int res, temp;
+        cin >> res;
+        for (int i = 1; i < n; i++)
+        {
+            cin >> temp;
+            res |= temp;
+            cout << res << " ";
+            res = temp;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
