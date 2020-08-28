f14e98eb4f8b44e263d479442ac80ac5cd93d318
Daniel Livingston <r.daniellivingston@gmail.com>
2020-08-28 22:15:24 +0530

    New Problem added

---
+++ Problems_Solved/GeeksForGeeks/Topic-wise-solution/Bit Magic/Grey Code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << (n ^ (n >> 1)) << endl;
+    }
+    return 0;
+}
