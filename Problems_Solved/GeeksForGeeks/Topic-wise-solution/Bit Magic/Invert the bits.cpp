7061da99d67989d8154c2f87aa7cbe641fbd9acd
Daniel Livingston <r.daniellivingston@gmail.com>
2020-08-28 22:14:35 +0530

    New Problem added

---
+++ Problems_Solved/GeeksForGeeks/Topic-wise-solution/Bit Magic/Invert the bits.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        unsigned int n;
+        cin >> n;
+        cout << (~n) << endl;
+    }
+    return 0;
+}
