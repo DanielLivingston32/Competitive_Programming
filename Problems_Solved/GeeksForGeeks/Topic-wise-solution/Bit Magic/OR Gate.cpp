3b7d77266f7808aeb4c86dd36eca163c96f00f0d
Daniel Livingston <r.daniellivingston@gmail.com>
2020-08-28 22:14:21 +0530

    New Problem added

---
+++ Problems_Solved/GeeksForGeeks/Topic-wise-solution/Bit Magic/OR Gate.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int res = 0, temp;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> temp;
+            res |= temp;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
