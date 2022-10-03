2e59b9f341a3bdeac97a963e8da5d3588497d2fe
RichardTimothy1307 <56349666+RichardTimothy1307@users.noreply.github.com>
2022-10-03 16:55:16 +0530

    Update all_distinct_b.cpp

--- Problems_Solved/Codeforces/B Problems/all_distinct_b.cpp
+++ Problems_Solved/Codeforces/B Problems/all_distinct_b.cpp
@@ -1,1 +1,89 @@
 
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2,fma")
+
+#include<bits/stdc++.h>
+
+//ordered set
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+// Header Files End
+
+using namespace std;
+using namespace __gnu_pbds;
+
+//typedef tree<pair<int, int>, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+// find_by_order --> find the element at the ith index
+// order_of_key --> find the element smaller than the parameter passed in the array
+//if you want to arrange the values in descending order replace less<int> with greater<int>
+//less_equal for multiple values
+
+
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+//ordered_set<pair<int, int>> A; , ordered<int> A;
+
+
+template<class key, class value, class cmp = std::less<key>>
+using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+
+
+#define int long long
+double pi = 3.14159265358979323846;
+#define  PB push_back
+#define  MP make_pair
+#define INF 1000000007
+const int mod = 1e9 + 7;
+
+//vector<int> graph[(int)1000];  //defining array of vectors
+//to_ullong()
+
+
+int case_ans = 1;
+
+//cout << "Case #" << case_ans << ": " << ans << endl;
+
+
+
+void solve() {
+
+	int n;
+	cin >> n;
+	set<int> s;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		s.insert(x);
+	}
+	if ((n - s.size()) % 2 == 0) {
+		cout << s.size() << endl;
+	}
+	else {
+		cout << s.size() - 1 << endl;
+	}
+}
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("ccallinput.txt", "r", stdin);
+	freopen("ccalloutput.txt", "w", stdout);
+#endif
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t = 1;
+	cin >> t;
+
+	while (t--) {
+		solve();
+		//case_ans++;
+	}
+
+	return 0;
+
+
+}
